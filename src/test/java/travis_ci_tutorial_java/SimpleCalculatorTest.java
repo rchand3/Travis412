0c3c4edb184c693911c29b58cdd385ed416a616e
rchand3 <48228975+rchand3@users.noreply.github.com>
2019-03-04 22:56:12 -0500

    Multiple test cases added

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -16,4 +16,25 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.minus(5, 3), 2);
 	}
+	@Test
+	public void testMultiply() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.multiply(2, 2), 4);
+	}
+	@Test
+	public void testDivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.divide(2, 2), 1);
+	}
+	@Test
+	public void testDivideByZero() {
+		SimpleCalculator calc = new SimpleCalculator();
+		try
+		{
+			calc.divide(2, 0);
+		} catch (Exception e) {
+		}
+		fail("Divide by zero");
+		
+	}
 }
